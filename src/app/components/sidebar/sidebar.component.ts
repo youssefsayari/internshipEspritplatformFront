8193a29f163eb80f8c95dba26b02cfbb3a2d243a
youssefsayari <youssef.sayari@esprit.tn>
2025-02-20 09:54:32 +0100

    template_admin

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -8,14 +8,14 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
     { path: '/dashboard', title: 'Dashboard',  icon: 'design_app', class: '' },
-    { path: '/icons', title: 'Icons',  icon:'education_atom', class: '' },
-    { path: '/maps', title: 'Maps',  icon:'location_map-big', class: '' },
-    { path: '/notifications', title: 'Notifications',  icon:'ui-1_bell-53', class: '' },
+    { path: '/icons', title: 'Post Management',  icon:'education_atom', class: '' },
+    { path: '/maps', title: 'Company Management',  icon:'location_map-big', class: '' },
+    { path: '/notifications', title: 'Internship Management',  icon:'ui-1_bell-53', class: '' },
 
-    { path: '/user-profile', title: 'User Profile',  icon:'users_single-02', class: '' },
-    { path: '/table-list', title: 'Table List',  icon:'design_bullet-list-67', class: '' },
-    { path: '/typography', title: 'Typography',  icon:'text_caps-small', class: '' },
-    { path: '/upgrade', title: 'Upgrade to PRO',  icon:'objects_spaceship', class: 'active active-pro' }
+    { path: '/user-profile', title: 'Convention Management',  icon:'users_single-02', class: '' },
+    { path: '/table-list', title: 'Task Management',  icon:'design_bullet-list-67', class: '' },
+    { path: '/typography', title: 'Defense Management',  icon:'text_caps-small', class: '' },
+    { path: '/upgrade', title: 'Make a Claim',  icon:'objects_spaceship', class: 'active active-pro' }
 
 ];
 
