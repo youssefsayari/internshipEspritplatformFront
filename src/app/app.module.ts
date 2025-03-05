2609dddf7f62d18470fd995071be4d68879e97bd
MedAminKaboubi <Kaboubi.Mohamedamin@esprit.tn>
2025-03-05 23:00:33 +0100

    document component imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -12,6 +12,15 @@ import { ComponentsModule } from './components/components.module';
 import { AppComponent } from './app.component';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { LoginComponent } from './components/login/login.component';
+import { DocumentComponent } from './document/document.component';
+import { DocumentListComponent } from './document-list/document-list.component';
+import { DefenseComponent } from './defense/defense.component';
+import { DocumentDetailsComponent } from './document-details/document-details.component';
+import { PredefinedDocumentsComponent } from './predefined-documents/predefined-documents.component';
+import { GenerateCvComponent } from './generate-cv/generate-cv.component';
+import { AddDocumentComponent } from './add-document/add-document.component';
+
 
 @NgModule({
   imports: [
@@ -22,11 +31,20 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     RouterModule,
     AppRoutingModule,
     NgbModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    ReactiveFormsModule
   ],
   declarations: [
     AppComponent,
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    LoginComponent,
+    DocumentComponent,
+    DocumentListComponent,
+    DefenseComponent,
+    DocumentDetailsComponent,
+    PredefinedDocumentsComponent,
+    GenerateCvComponent,
+    AddDocumentComponent
 
   ],
   providers: [],
