de24384c892737e68cef4f487486988f59952dc1
youssefsayari <youssef.sayari@esprit.tn>
2025-03-05 13:44:38 +0100

    aa

--- src/app/components/icons/icons.component.ts
+++ src/app/components/icons/icons.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewEncapsulation } from '@angular/core';  // Import ViewEncapsulation from @angular/core
 
 @Component({
   selector: 'app-icons',
   templateUrl: './icons.component.html',
-  styleUrls: ['./icons.component.css']
+  styleUrls: ['./icons.component.css'],
+  encapsulation: ViewEncapsulation.None,  // Use the enum correctly
 })
 export class IconsComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  image: string | undefined;
+  from: string | undefined;
+  showProfileCard: boolean = false;  // Nouvelle variable pour contrôler l'affichage du profile card
+  // Méthode pour mettre à jour les informations du profil
+  onProfileSelected(profileData: any) {
+    this.image = profileData.image;
+    this.from = profileData.from;
+    this.showProfileCard = true;  // Affiche le profile card
+    
   }
+
+
+
+  
+
+
+
+
+
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+ 
 
 }
